e4d0ca5580e8040a838b3e571128a2629b4a4b6e
paramighty <avinnosatta@gmail.com>
2024-04-19 13:12:59 +0200

    Merged the icon component with the  Buttons component. This modification ensures the component can handle both scenarios—buttons with and without icons. Added icons to public.

--- app/components/buttons.js
+++ app/components/buttons.js
@@ -1,7 +1,18 @@
-export default function Buttons({ className = "", onClick, children }) {
+import Image from "next/image";
+
+export default function Buttons({
+	className = "",
+	onClick,
+	ctaBtn,
+	icon,
+	alt,
+	src,
+	children,
+}) {
 	return (
-		<button type="button" className={`${className}`} onClick={onClick}>
-			{children}
+		<button className={`${className}`} onClick={onClick}>
+			{ctaBtn && children}
+			{icon && <Image src={src} alt={alt} height={25} width={25} />}
 		</button>
 	);
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
+"use client";
 import Image from "next/image";
+import Buttons from "./components/buttons";
+import Navbar from "./molecules/navbar";
+import twitter from "/public/icons/twitterIcon.webp";
 
 export default function Home() {
 	return (
 		<main className="">
+			<Navbar />
 			<div className="w-full font-druk"> does this work?</div>
+			<Buttons
+				children="Search"
+				icon={false}
+				ctaBtn={true}
+				className="btn-CTA"
+				onClick={() => console.log("open modal")}
+			/>
+			<Buttons
+				icon={true}
+				src={twitter}
+				onClick={() => print("Done icon+button")}
+			/>
 		</main>
 	);
 }

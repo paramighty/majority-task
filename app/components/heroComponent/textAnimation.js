0cef99fdfdc7360b04af3663fc2c4f94e1e27045
paramighty <avinnosatta@gmail.com>
2024-04-20 21:30:36 +0200

    created heroComponent text animation with framer motion

---
+++ app/components/heroComponent/textAnimation.js
@@ -0,0 +1,48 @@
+"use client";
+import { useState, useEffect } from "react";
+import { AnimatePresence, easeInOut, motion } from "framer-motion";
+
+export default function TextAnimation() {
+	const words = [
+		["MOROCCO?"],
+		["BANLADESH?"],
+		["USA?"],
+		["HONGKONG?"],
+		["SWEDEN?"],
+		["SAMOA?"],
+		["END OR WORLD?"],
+	];
+
+	const [index, setIndex] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setIndex((pv) => (pv + 1) % words.length);
+		}, 3000);
+		return () => clearInterval(interval);
+	}, []);
+
+	return (
+		<div className="flex justify-center content-center">
+			<AnimatePresence mode="wait">
+				<motion.h1
+					key={words[index]}
+					initial={{ opacity: 1, y: 50 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{
+						duration: 0.4,
+						ease: "easeInOut",
+						delay: 0.01,
+					}}
+					exit={{
+						opacity: 0,
+						y: -50,
+					}}
+					className="font-druk text-[5rem] md:text-[6rem] lg:text-[7.5rem] text-black"
+				>
+					{words[index]}
+				</motion.h1>
+			</AnimatePresence>
+		</div>
+	);
+}

59de38c46e4f92a3734b116306e6d98c074d4edb
paramighty <avinnosatta@gmail.com>
2024-04-23 00:53:29 +0200

    Minor CSS

--- app/page.js
+++ app/page.js
@@ -4,10 +4,12 @@ import Navbar from "./components/navbar/navbar";
 import HeroComponent from "./components/heroComponent/heroComponent";
 import { MyContext, MyContextProvider } from "./context/context";
 import { useContext } from "react";
+import Footer from "./components/footer/footer";
+import SearchBar from "./components/heroComponent/searchBar";
 
 export default function Home() {
 	return (
-		<main className="flex flex-col justify-center content-center min-h-screen">
+		<main className="flex flex-col justify-center content-center">
 			<Navbar />
 			<HeroComponent />
 		</main>

8212c5997b75b1be87695557b7cb8c71d88f61bf
paramighty <avinnosatta@gmail.com>
2024-04-23 22:47:38 +0200

    More readable code by defining variables

--- app/country/[slug]/countryContinent.js
+++ app/country/[slug]/countryContinent.js
@@ -17,11 +17,21 @@ export default function CountryContinent() {
 	const country = myState?.selectedCountry;
 
 	if (!country?.name?.common) {
-		return <></>;
+		return null;
 	}
 
 	const languageNames = Object.values(country?.languages || {});
-	console.log(`${languageNames}`);
+	const regionDetails = country?.subregion || "mystry";
+	const demonyms = country?.demonyms?.eng?.f || "your friend";
+	const hasWaterbody = country?.landlocked ? "do not have" : "has";
+	const relativeToWater = country?.landlocked ? "unlike" : "like";
+	const driveSide = country?.car?.side || "unknown";
+	const borderingCountries = country?.borders?.length
+		? `${country.borders.length} neighbours`
+		: "no neighbours";
+	const languageText = languageNames.length
+		? `People speak ${languageNames[0]} here and so remember to learn a word or two in ${languageNames[0]}.`
+		: "There is no specific language that people speak here.";
 
 	if (country.subregion === "Africa") {
 		console.log("it is a great match");
@@ -41,25 +51,19 @@ export default function CountryContinent() {
 
 					<div className="grid grid-cols-6 md:max-w-[50%] md:min-w-[50%]">
 						<h4 className="h4 leading-none font-druk gap-4 col-start-2 col-span-4 text-center">
-							{country?.name?.common}, a country in{" "}
-							{country?.subregion || "mystry"}
+							{country?.name?.common}, a country in {regionDetails}
 						</h4>
 						<p className="p font-gta col-start-2 col-span-4 text-center">
 							If you meet someone from {country?.name?.common}, you can refer to
-							them as {country?.demonyms?.eng?.f}. The country{" "}
-							{country?.landlocked?.true ? "do not have" : "has"} its waterbody{" "}
-							{country?.landlocked?.true ? "unlike" : "like"} most other
-							countries. It shares border with{" "}
-							{country?.borders?.length
-								? `${country?.borders?.length} neighbours`
-								: "no neighbours"}
-							.
-							{languageNames.length
-								? ` People speak ${languageNames[0]} here and so remember to learn a
-								word or two in ${languageNames[0]}.`
-								: " There is no specific language that people speak here."}{" "}
-							And, important - they drive on the {country?.car?.side} side of
-							the road. If you are not comfortable, better to hire a chauffer.
+							them as {demonyms}. The country {hasWaterbody} its waterbody{" "}
+							{relativeToWater} most other countries. It shares border with
+							{borderingCountries}.{languageText}
+							And, important -{" "}
+							{country?.car
+								? `they drive on the ${country?.car.side} side of
+							the road.`
+								: "they don't have much roads there."}{" "}
+							If you are not comfortable, better to hire a chauffer.
 						</p>
 					</div>
 				</div>

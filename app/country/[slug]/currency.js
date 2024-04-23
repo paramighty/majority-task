fa6b4fcc28add6071593971fbb9c6c6c2c46061b
paramighty <avinnosatta@gmail.com>
2024-04-23 14:04:53 +0200

    Added CSS to make the component have universal styling

--- app/country/[slug]/currency.js
+++ app/country/[slug]/currency.js
@@ -17,14 +17,19 @@ export default function Currency() {
 	const currencyNames = Object.values(country.currencies);
 
 	return (
-		<div className="relative bg-black max-h-screen">
-			<div className="px-0 h-full m-auto w-screen max-w-[1680px]">
+		<div className="relative bg-black min-h-fit">
+			<div className="p-4 md:px-0 h-full m-auto w-screen max-w-[1680px]">
 				<div className="flex min-h-screen flex-col justify-center items-center">
-					<h4 className="h4 font-druk text-white">MONEY MATTERS</h4>
-					<p className="p font-gta text-white">
+					<h4 className="h4 font-druk text-white text-center">MONEY MATTERS</h4>
+					<p className="p font-gta text-white text-center">
 						Their currency is called{" "}
 						<em className="text-[#BEF300]">{currencyNames[0].name}</em>. Watch
-						out for the symbol "{currencyNames[0].symbol}" for money
+						out for the symbol{" "}
+						<strong className="text-[#794DFF]">
+							{" "}
+							{currencyNames[0].symbol}{" "}
+						</strong>{" "}
+						for money
 					</p>
 				</div>
 			</div>

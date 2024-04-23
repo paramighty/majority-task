10085651e5cc64c955d944e74aed07b0878e96be
paramighty <avinnosatta@gmail.com>
2024-04-24 00:40:17 +0200

    Added conditional rendering for mao of continents

--- app/country/[slug]/countryContinent.js
+++ app/country/[slug]/countryContinent.js
@@ -2,13 +2,8 @@
 
 import { useContext, useEffect } from "react";
 import { MyContext } from "../../context/context";
-import mapAfrica from "/public/images/Map-of-Continent-Africa.webp";
-import mapNorthAmerica from "/public/images/Map-of-North-America.webp";
-import mapAsia from "/public/images/Map-of-Asia.webp";
-import mapOceania from "/public/images/Map-of-Australia.webp";
-import mapEurope from "/public/images/Map-of-Europe.webp";
-import mapAntarctica from "/public/images/Map of Antarctica.png";
-import mapSouthAmerica from "/public/images/Map of Antarctica.png";
+
+import MapRendering from "./mapRendering";
 
 import Image from "next/image";
 
@@ -25,7 +20,10 @@ export default function CountryContinent() {
 	const demonyms = country?.demonyms?.eng?.f || "your friend";
 	const hasWaterbody = country?.landlocked ? "do not have" : "has";
 	const relativeToWater = country?.landlocked ? "unlike" : "like";
-	const driveSide = country?.car?.side || "unknown";
+	const driveSide = country?.car?.side
+		? `they drive on the ${country?.car.side} side of
+	the road`
+		: "they don't have much roads there";
 	const borderingCountries = country?.borders?.length
 		? `${country.borders.length} neighbours`
 		: "no neighbours";
@@ -37,17 +35,13 @@ export default function CountryContinent() {
 		console.log("it is a great match");
 	}
 
+	console.log(`${country?.region}`);
+
 	return (
 		<div className="relative bg-[#DBCCFC] min-h-fit">
 			<div className="p-4 md:px-0 h-full m-auto w-screen max-w-[1680px]">
 				<div className="flex min-h-screen flex-col md:flex-row justify-center">
-					<div className="grid md:max-w-[50%] md:min-w-[50%] grid-cols-1 md:grid-cols-6 ">
-						<Image
-							className="md:col-start-2 md:col-span-4 self-center justify-self-center"
-							src={mapAfrica}
-							alt="map of the continent"
-						/>
-					</div>
+					<MapRendering />
 
 					<div className="grid grid-cols-6 md:max-w-[50%] md:min-w-[50%]">
 						<h4 className="h4 leading-none font-druk gap-4 col-start-2 col-span-4 text-center">
@@ -57,12 +51,8 @@ export default function CountryContinent() {
 							If you meet someone from {country?.name?.common}, you can refer to
 							them as {demonyms}. The country {hasWaterbody} its waterbody{" "}
 							{relativeToWater} most other countries. It shares border with
-							{borderingCountries}. {languageText}. And, important -{" "}
-							{country?.car
-								? `they drive on the ${country?.car.side} side of
-							the road.`
-								: "they don't have much roads there."}{" "}
-							If you are not comfortable, better to hire a chauffer.
+							{borderingCountries}. {languageText}. And, important - {driveSide}
+							. If you are not comfortable, better to hire a chauffer.
 						</p>
 					</div>
 				</div>
---
+++ app/country/[slug]/mapRendering.js
@@ -0,0 +1,69 @@
+import { useContext } from "react";
+import { MyContext } from "../../context/context";
+import Image from "next/image";
+import mapAfrica from "/public/images/Map-of-Continent-Africa.webp";
+import mapNorthAmerica from "/public/images/Map-of-North-America.webp";
+import mapAsia from "/public/images/Map-of-Asia.webp";
+import mapOceania from "/public/images/Map-of-Australia.webp";
+import mapEurope from "/public/images/Map-of-Europe.webp";
+import mapAntarctica from "/public/images/Map-of-Antarctica.webp";
+import mapSouthAmerica from "/public/images/Map-of-South-America.webp"; // Correct this path if it's incorrect
+
+export default function MapRendering() {
+	const { myState } = useContext(MyContext);
+	const country = myState?.selectedCountry;
+
+	if (!country?.name?.common) {
+		return null;
+	}
+	const region = country?.region;
+	console.log(`${region}`);
+
+	return (
+		<div className="grid md:max-w-[50%] md:min-w-[50%] grid-cols-1 md:grid-cols-6">
+			{region === "Africa" ? (
+				<Image
+					className="md:col-start-2 md:col-span-4 self-center justify-self-center rounded-lg shadow-2xl"
+					src={mapAfrica}
+					alt="map of the continent"
+				/>
+			) : region === "Europe" ? (
+				<Image
+					className="md:col-start-2 md:col-span-4 self-center justify-self-center rounded-lg shadow-2xl"
+					src={mapEurope}
+					alt="map of the continent"
+				/>
+			) : region === "Asia" ? (
+				<Image
+					className="md:col-start-2 md:col-span-4 self-center justify-self-center rounded-lg shadow-2xl"
+					src={mapAsia}
+					alt="map of the continent"
+				/>
+			) : region === "America" ? (
+				<Image
+					className="md:col-start-2 md:col-span-4 self-center justify-self-center rounded-lg shadow-2xl"
+					src={mapNorthAmerica}
+					alt="map of the continent"
+				/>
+			) : region === "Americas" ? (
+				<Image
+					className="md:col-start-2 md:col-span-4 self-center justify-self-center rounded-lg shadow-2xl"
+					src={mapSouthAmerica}
+					alt="map of the continent"
+				/>
+			) : region === "Oceania" ? (
+				<Image
+					className="md:col-start-2 md:col-span-4 self-center justify-self-center rounded-lg shadow-2xl"
+					src={mapOceania}
+					alt="map of the continent"
+				/>
+			) : region === "Antarctica" ? (
+				<Image
+					className="md:col-start-2 md:col-span-4 self-center justify-self-center rounded-lg shadow-2xl"
+					src={mapAntarctica}
+					alt="map of the continent"
+				/>
+			) : null}
+		</div>
+	);
+}
